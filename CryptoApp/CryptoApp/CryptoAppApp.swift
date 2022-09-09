08867dc274d1a32c1e3058450d83461f856ea91b
Tawhidul Arefin <tawhidularefin530@gmail.com>
2022-09-09 17:07:27 +0600

    Initialized: The App

---
+++ CryptoApp/CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Md Najmuzzaman on 9/9/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
